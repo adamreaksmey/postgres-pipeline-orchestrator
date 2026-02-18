df883d34a92b0259d7b0668fef22a54a1692c9b9
Adam - The Developer <adaminiature@gmail.com>
2026-02-18 11:23:39 +0700

    feat: add RUN_WORKER_LOOP environment variable and log its status on application startup

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,6 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Swagger: http://localhost:${port}/${swaggerPath}`);
+  console.log(`Worker loop run status: ${process.env.RUN_WORKER_LOOP}`);
 }
 bootstrap();
