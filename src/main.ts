e5e87b9fbbe8a0a63efe883c1903476a09b1a5df
Adam - The Developer <adaminiature@gmail.com>
2026-02-18 11:06:46 +0700

    chore: refactor app listening logic to store port in a variable and log Swagger URL on startup

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(swaggerPath, app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Swagger: http://localhost:${port}/${swaggerPath}`);
 }
 bootstrap();
